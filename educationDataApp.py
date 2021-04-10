c04a6668bcc1c26a29585d5a7ba5e0dcc908bc78
Bruno Žitković <brunozitkovic@gmail.com>
2021-04-10 15:04:41 +0200

    added boxplot and new gui features

--- educationDataApp.py
+++ educationDataApp.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Student:
-    def __init__(self, gender, nationality, placeOfBirth, subject, raisedHands, visitedResources, viewedAnnouncements):
+    def __init__(self, gender, nationality, placeOfBirth, subject, raisedHands, visitedResources, viewedAnnouncements, gradeClass):
         self.gender = gender
         self.nationality = nationality
         self.placeOfBirth = placeOfBirth
@@ -13,6 +13,7 @@ class Student:
         self.raisedHands = raisedHands
         self.visitedResources = visitedResources
         self.viewedAnnouncements = viewedAnnouncements
+        self.gradeClass = gradeClass
     
     def GetStudentInfo(self):
         return f'\nGender: {self.gender} \
@@ -20,7 +21,8 @@ class Student:
             \nPlace of birth: {self.placeOfBirth} \
             \nRaised hands: {self.raisedHands} \
             \nVisited resources: {self.visitedResources} \
-            \nViewed announcements: {self.viewedAnnouncements}'
+            \nViewed announcements: {self.viewedAnnouncements} \
+            \nGrade class: {self.gradeClass}'
     
 class DataConverter():
     @staticmethod   
@@ -38,8 +40,9 @@ class DataConverter():
                 raisedHands = value['raisedhands']
                 visitedResources = value['VisITedResources']
                 viewedAnnouncements = value['AnnouncementsView']
+                gradeClass = value['Class']
 
-                student = Student(gender, nationality, placeOfBirth, subject, raisedHands, visitedResources, viewedAnnouncements)
+                student = Student(gender, nationality, placeOfBirth, subject, raisedHands, visitedResources, viewedAnnouncements, gradeClass)
                 students.append(student)
         
         return students
@@ -94,7 +97,8 @@ class Calculations():
             student.subject, 
             student.raisedHands,
             student.visitedResources,
-            student.viewedAnnouncements))
+            student.viewedAnnouncements,
+            student.gradeClass))
         
         NewWindow(values=studentsInfo)     
 
@@ -143,14 +147,28 @@ class Calculations():
             raisedHands.append(int(student.raisedHands))
             visitedResources.append(int(student.visitedResources))
         
-        ChartDrawing.DrawBoxPlot(raisedHands, visitedResources, 'Comparison between No. raised hands and visited resources')
+        ChartDrawing.DrawBoxPlot([raisedHands, visitedResources], 'Comparison between No. raised hands and visited resources')
 
+    @staticmethod
+    def GetShareOfStudenGradeClass(students):
+        lowClass = 0
+        middleClass = 0
+        highClass = 0
+
+        for student in students:
+            if(student.gradeClass == 'L'):
+                lowClass += 1
+            elif(student.gradeClass == 'M'):
+                middleClass += 1
+            else:
+                highClass += 1
+        
+        ChartDrawing.DrawBarChart([lowClass, middleClass, highClass], ('low', 'medium', 'high'), 'Share of students by class grade')
 
 class ChartDrawing():
     @staticmethod
     def DrawPieChart(items, labels, title):
         y = np.array(items)
-        labels = labels
 
         fig, ax = plt.subplots()
 
@@ -161,13 +179,26 @@ class ChartDrawing():
         plt.show() 
 
     @staticmethod
-    def DrawBoxPlot(value1, value2, title):
-        data = [value1, value2]
-
+    def DrawBoxPlot(data, title):       
         fig, ax = plt.subplots()
 
         ax.set_title('Comparison between No. raised hands and visited resources')
         ax.boxplot(data)
+        fig.canvas.set_window_title(title)
+
+        plt.show()
+
+    @staticmethod
+    def DrawBarChart(data, labels, title):
+        fig, ax = plt.subplots()    
+
+        x_pos = np.arange(len(labels))
+        y_pos = np.arange(len(labels))
+
+        ax.bar(x_pos, data, align='center', alpha=0.7, color='green')
+        ax.set_xticks(y_pos)
+        ax.set_xticklabels(labels)
+        ax.set_ylabel('Number of students')
         fig.canvas.set_window_title(title)
 
         plt.show()
@@ -179,62 +210,85 @@ class GUI():
         window.title('Education info')
         
         entNumberRaisedHands = tk.Entry(master=window, width=15)
-        entNumberRaisedHands.grid(row=1, column=0, pady=10)
+        entNumberRaisedHands.grid(row=1, column=0, padx=25, pady=10, sticky='W')
 
-        lblNumber = tk.Label(master=window, text='Number of raised hands:')
-        lblNumber.grid(row=0, column=0, padx=20)       
+        lblNumber = tk.Label(master=window, font=("Ariel", 12), bg='#7878ab', text='Number of raised hands:')
+        lblNumber.grid(row=0, column=0, padx=20, sticky='W')       
 
         btnGetNumberOfHands = tk.Button(
             master = window,
             text = '➔',
-            width = 5,
+            width = 6,
+            bd = 3,
+            bg = '#78ab7c',
             command = lambda: Calculations.GetRaisedHandsForAboveSetNumber(entNumberRaisedHands.get(), students)
         )
         btnGetNumberOfHands.grid(row=1, column=1)
 
-        lblGetStudentInfo = tk.Label(master=window, text='Get all students info')
-        lblGetStudentInfo.grid(row=2, column=0, padx=20, pady=10)
+        lblGetStudentInfo = tk.Label(master=window, font=("Ariel", 12), bg='#7878ab', text='Get all students info')
+        lblGetStudentInfo.grid(row=2, column=0, padx=20, pady=10, sticky='W')
 
         btnGetStudentInfo = tk.Button(
             master = window,
             text = '➔',
-            width = 5,
+            width = 6,
+            bd = 3,
+            bg = '#78ab7c',
             command = lambda: Calculations.GetStudentsInfo(students)
         )
         btnGetStudentInfo.grid(row=2, column=1)      
 
-        lblGetShareRaisedHandsByGender = tk.Label(master=window, text='Get share for raised hands by gender')
-        lblGetShareRaisedHandsByGender.grid(row=3, column=0, padx=20, pady=10)
+        lblGetShareRaisedHandsByGender = tk.Label(master=window, font=("Ariel", 12), bg='#7878ab', text='Get share for raised hands by gender')
+        lblGetShareRaisedHandsByGender.grid(row=3, column=0, padx=20, pady=10, sticky='W')
 
         btnGetShareRaisedHandsByGender = tk.Button(
             master = window,
-            text = '➔',
-            width = 5,
+            text = '➔',            
+            width = 6,
+            bd = 3,
+            bg = '#78ab7c',
             command = lambda: Calculations.GetMaleFemaleShareForRaisedHands(students)
         )
         btnGetShareRaisedHandsByGender.grid(row=3, column=1)
 
-        lblGetShareOfSubjects = tk.Label(master=window, text='Get share of subjects')
-        lblGetShareOfSubjects.grid(row=4, column=0, padx=20, pady=10)
+        lblGetShareOfSubjects = tk.Label(master=window, font=("Ariel", 12), bg='#7878ab', text='Get share of subjects')
+        lblGetShareOfSubjects.grid(row=4, column=0, padx=20, pady=10, sticky='W')
 
         btnGetShareOfSubjects = tk.Button(
             master = window,
             text = '➔',
-            width = 5,
+            width = 6,
+            bd = 3,
+            bg = '#78ab7c',
             command = lambda: Calculations.GetSubjectShare(students)
         )
         btnGetShareOfSubjects.grid(row=4, column=1, padx=20, pady=10)
 
-        lblGetComparisonBetweenHandsAndResources = tk.Label(master=window, text='Comparison between raised hand and visited resources')
-        lblGetComparisonBetweenHandsAndResources.grid(row=5, column=0, padx=20, pady=10)
+        lblGetComparisonBetweenHandsAndResources = tk.Label(master=window, font=("Ariel", 12), bg='#7878ab', text='Comparison between raised hand and visited resources')
+        lblGetComparisonBetweenHandsAndResources.grid(row=5, column=0, padx=20, pady=10, sticky='W')
 
         btnlblGetComparisonBetweenHandsAndResources = tk.Button(
             master = window,
             text = '➔',
-            width = 5,
+            width = 6,
+            bd = 3,
+            bg = '#78ab7c',
             command = lambda: Calculations.GetComparisonBetweenHandsAndResources(students)
         )
         btnlblGetComparisonBetweenHandsAndResources.grid(row=5, column=1, padx=20, pady=10)
+
+        lblGetShareOfStudenGradeClass = tk.Label(master=window, font=("Ariel", 12), bg='#7878ab', text='Share of students by class grade')
+        lblGetShareOfStudenGradeClass.grid(row=6, column=0, padx=20, pady=10, sticky='W')
+
+        btnGetShareOfStudenGradeClass = tk.Button(
+            master = window,
+            text = '➔',
+            width = 6,
+            bd = 3,
+            bg = '#78ab7c',
+            command = lambda: Calculations.GetShareOfStudenGradeClass(students)
+        )
+        btnGetShareOfStudenGradeClass.grid(row=6, column=1, padx=20, pady=10)
 
         return window
     
@@ -243,17 +297,20 @@ class NewWindow(tk.Toplevel):
     def __init__(self, master = None, text = '', values = []):
         super().__init__(master = master)
         self.title('Calculation info')
-        self.geometry('300x100')
+        self.geometry('380x80')
+        self.configure(bg='#7878ab')
         self.values = values
-        label = tk.Label(self, text = text)
-        label.grid(row=0, column=0, padx=15)
+
+        label = tk.Label(self, font=("Ariel", 12), bg='#7878ab', text = text)
+        label.grid(row=0, column=0, padx=30, sticky='w')
+
         if(len(values) != 0):
             self.SetTreeTable(values)
            
     def SetTreeTable(self, values):       
-        self.geometry('1000x300')
+        self.geometry('1100x300')
         self.tree = ttk.Treeview(self)
-        self.tree['columns']=('Gender',"Nationality",'Place of Birth', 'Subject', 'No. Raised hands', 'No. Visited resources', 'No. Viewed Announcements')
+        self.tree['columns']=('Gender',"Nationality",'Place of Birth', 'Subject', 'No. Raised hands', 'No. Visited resources', 'No. Viewed Announcements', 'Class')
         self.tree.column('#0', width=80, minwidth=50, stretch=tk.NO)
         self.tree.column('Gender', width=80, minwidth=50, stretch=tk.NO)
         self.tree.column('Nationality', width=80, minwidth=50)
@@ -262,15 +319,17 @@ class NewWindow(tk.Toplevel):
         self.tree.column('No. Raised hands', width=150, minwidth=200, stretch=tk.NO)
         self.tree.column('No. Visited resources', width=150, minwidth=200, stretch=tk.NO)
         self.tree.column('No. Viewed Announcements', width=160, minwidth=200, stretch=tk.NO)
+        self.tree.column('Class', width=80, minwidth=50, stretch=tk.NO)
 
-        self.tree.heading('#0',text="Name",anchor=tk.W)
-        self.tree.heading('Gender', text='Gender',anchor=tk.W)
-        self.tree.heading('Nationality', text='Nationality',anchor=tk.W)
-        self.tree.heading('Place of Birth', text='Place of birth',anchor=tk.W)
-        self.tree.heading('Subject', text='Subject',anchor=tk.W)
-        self.tree.heading('No. Raised hands', text='No. Raised hands',anchor=tk.W)
-        self.tree.heading('No. Visited resources', text='No. Visited resources',anchor=tk.W)
-        self.tree.heading('No. Viewed Announcements', text='No. Viewed Announcements',anchor=tk.W)
+        self.tree.heading('#0', text="Name", anchor=tk.W)
+        self.tree.heading('Gender', text='Gender', anchor=tk.W)
+        self.tree.heading('Nationality', text='Nationality', anchor=tk.W)
+        self.tree.heading('Place of Birth', text='Place of birth', anchor=tk.W)
+        self.tree.heading('Subject', text='Subject', anchor=tk.W)
+        self.tree.heading('No. Raised hands', text='No. Raised hands', anchor=tk.W)
+        self.tree.heading('No. Visited resources', text='No. Visited resources', anchor=tk.W)
+        self.tree.heading('No. Viewed Announcements', text='No. Viewed Announcements', anchor=tk.W)
+        self.tree.heading('Class', text='Class', anchor=tk.W)
 
         for val in values:
             self.tree.insert(parent='', index='end', text="Student", values=val)
@@ -281,7 +340,10 @@ def main():
     students = DataConverter.ConvertJsonToObject('eduData.json')
 
     gui = GUI().SetGUI(students)
-    gui.geometry('450x250')
+
+    gui.geometry('550x300')
+    gui.configure(bg='#7878ab')
+
     gui.mainloop()
 
 main()
